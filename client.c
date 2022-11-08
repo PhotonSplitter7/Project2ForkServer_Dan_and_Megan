9fffa0ea35b9ccc52c0926afd44cad500d740a2e
PhotonSplitter7 <70785987+PhotonSplitter7@users.noreply.github.com>
2022-11-08 09:31:12 -0500

    Add files via upload
    Needs parent to listen for socket data and child to wait for user input

--- client.c
+++ client.c
@@ -5,7 +5,11 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 
+//TODO after printing message in client window, print a bunch of spaces "                " of multiple lines to clear out below
 #define MAXLINE 4096 /*max text line length*/
 #define SERV_PORT 3000 /*port*/
 
@@ -15,6 +19,14 @@ main(int argc, char **argv)
  int sockfd;
  struct sockaddr_in servaddr;
  char sendline[MAXLINE], recvline[MAXLINE];
+ //student variables****
+ pid_t child = 1;
+ //pipe setup for communicating with child client 1)child client listens for input for user, blocking. 2) parent loops non blocking waiting for either socket data from server or pipe data
+ //from child client
+ int p1[2];
+ pipe(p1);
+ fcntl(p1[0], F_SETFL, O_NONBLOCK);//non blocking assignment
+ fcntl(p1[1], F_SETFL, O_NONBLOCK);//non blocking assignment
 	
  //basic check of the arguments
  //additional checks can be inserted
@@ -41,19 +53,52 @@ main(int argc, char **argv)
   perror("Problem in connecting to the server");
   exit(3);
  }
-	
- while (fgets(sendline, MAXLINE, stdin) != NULL) {
-	
-  send(sockfd, sendline, strlen(sendline), 0);
-		
-  if (recv(sockfd, recvline, MAXLINE,0) == 0){
-   //error: server terminated prematurely
-   perror("The server terminated prematurely"); 
-   exit(4);
-  }
-  printf("%s", "String received from the server: ");
-  fputs(recvline, stdout);
+
+ //student code ************************
+ fcntl(sockfd, F_SETFL, O_NONBLOCK);//make socket non blocking
+ //fork to parent and child
+ child = fork();
+
+
+ //parent process
+ if(child > 0){
+ //close pipe to write, read only
+ close(p1[1]);
+
+ while(1){
+
+ //read input MOVE TO CHILD
+ 	fgets(sendline, MAXLINE, stdin);
+
+ 	//send to server
+   send(sockfd, sendline, strlen(sendline), 0);
+
+   if (recv(sockfd, recvline, MAXLINE,0) == 0){
+    //error: server terminated prematurely
+    perror("The server terminated prematurely");
+    exit(4);
+   }
+   printf("%s", "String received from the server: ");
+   fputs(recvline, stdout);
  }
+
+ }
+
+//child process to wait for user input
+if(child == 0){
+//close pipe to read, write only
+close(p1[0]);
+while(1){
+char child_buf[MAXLINE];
+//read input
+fgets(child_buf, MAXLINE, stdin);
+write(p1[1],child_buf,MAXLINE);
+}
+}
+
+
+
+
 
  exit(0);
 }
